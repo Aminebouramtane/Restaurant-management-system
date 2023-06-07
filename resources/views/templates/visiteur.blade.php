2aacbaf307cd2cd8b66c4f4ca4d5a63af07d4367
amine <bouramtaneamine1@gmail.com>
2023-06-07 23:34:08 +0100

    add auth to project

--- resources/views/templates/visiteur.blade.php
+++ resources/views/templates/visiteur.blade.php
@@ -32,15 +32,15 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Localisation et horaires</a>
         </li>
-       
+
         <li class="nav-item">
           <a class="nav-link" href="#">Contact</a>
         </li>
          <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
+          <a class="nav-link" href="{{route('login')}}">Login</a>
         </li>
       </ul>
-      
+
     </div>
   </div>
 </nav>
